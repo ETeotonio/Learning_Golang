0f8e283f3a5a5a8e26985e40b98b54fd0b4b8ce3
Eliel <eliel.teotonio@gmail.com>
2022-08-15 10:13:01 +0100

    Update main.go
    Creating config file, added config yaml unmarshal, check if the file is available

--- API_Consume/main.go
+++ API_Consume/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
-type config_api struct {
+type configapi struct {
 	apiKey     string
 	apiAddress string
 
@@ -12,21 +17,42 @@ type config_api struct {
 	dbUri string
 }
 
+var data = make(map[string]configapi)
+var config_file = "config.yml"
+
 func main() {
-	if os.Args[1] == "createconfig" {
+	_, err := os.Stat(config_file)
+	if (os.Args[1] == "createconfig") || errors.Is(err, os.ErrNotExist) {
+		fmt.Println("creating config file...")
 		createConfigFile()
+	} else {
+
 	}
 }
 
 func createConfigFile() {
-	config := "API:\n"
-	config += "\tapiKey:\n"
-	config += "\tapiAddress:\n"
-	config += "DB:\n"
-	config += "\tdbKey:\n"
-	config += "\tdbUri:"
-	err := os.WriteFile("config.yml", []byte(config), 0644)
+	config := "apiKey:\n"
+	config += "apiAddress:\n"
+	config += "dbKey:\n"
+	config += "dbUri:"
+	err := os.WriteFile(config_file, []byte(config), 0644)
 	if err != nil {
 		panic(err)
 	}
 }
+
+func getConfig() {
+	config_file, err := ioutil.ReadFile(config_file)
+	if err != nil {
+		panic(err)
+	}
+	err2 := yaml.Unmarshal(config_file, &data)
+	if err2 != nil {
+		panic(err2)
+	}
+
+}
+
+// func getTickerPrice string(ticker string){
+// 	return ""
+// }
