7dd8672903abc9c61e83bac079c3a57078005f40
Eliel <eliel.teotonio@gmail.com>
2022-08-18 08:32:45 +0100

    Finnhub websocket connection

--- API_Consume/api_get.go
+++ API_Consume/api_get.go
@@ -1,5 +1,15 @@
 package main
 
-func getTickerPrice(apikey string, apiaddress string) {
+import (
+	"github.com/gorilla/websocket"
+)
 
+//https://finnhub.io/docs/api/websocket-trades
+func getTickerPrice(apikey string, apiaddress string) {
+	formatted_websocket_address := apiaddress + "?token=" + apikey
+	websock, err := websocket.DefaultDialer.Dial(formatted_websocket_address, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer websock.Close()
 }
